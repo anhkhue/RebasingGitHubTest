8fcaa76fe9deb0a9863e7e285a35e23698301d0b
tlakt <tlakt@KhueTLASE62541.fus.edu.vn>
2017-03-16 13:59:35 +0700

    Rebasing class constructors

--- GitHubRebaseTest/src/githubrebasetest/Rebasing.java
+++ GitHubRebaseTest/src/githubrebasetest/Rebasing.java
@@ -3,13 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package githubrebasetest;
 
 /**
- * 
+ *
  * @author KhueTLA
  */
 public class Rebasing {
-    
+
+    private String name;
+    private String feature;
+
+    public Rebasing() {
+    }
+
+    public Rebasing(String name, String feature) {
+        this.name = name;
+        this.feature = feature;
+    }
+
 }
